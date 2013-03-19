6ad6a2aa35f442ff914d1670febe908cac929ce0
jan croonen <info@jcroonen.nl>
2013-03-19 11:14:53 +0100

    v

--- jquery/jquery.sessietimer.js
+++ jquery/jquery.sessietimer.js
@@ -1,118 +1,127 @@
 /**
  * jquery.sessietimer.js
- * 
+ *
  * Plugin die een timer start om de sessieduur te spiegelen, d.w.z. aan de clientkant
  * een zelfde duur bij te houden als aan de serverkant, bijv. 15 minuten.
- * 
+ *
  * Als een grenswaarde overschreden wordt, verschijnt een dialoog met een
  * waarschuwingstekst, en een aflopende teller.
- * 
+ *
+ * Voor de dialoog die wordt aangeroepen is de jQuery UI bibliotheek noodzakelijk.
+ *
  * Als de sessieduur verstreken is, verdwijnt de waarschuwing als die nog zichtbaar is,
  * en wordt een callback aangeroepen, bijv. om de uitloglink te verbergen.
- * 
+ *
  * author: Jan Croonen
  * date: 10/2012
  */
-(function($) {
-  $.fn.sessietimer = function(options, verlengen, warnDialogContent, warnDialogTitle) {
-    var settings = {
-      'bijwerkInterval': 1, // in seconden
-      'sessieDuur': 15,  // in minuten
-      'waarschuwingsGrens': 3, // in minuten (0 = niet waarschuwen)
-      'sessieAanBoodschap': 'Uw sessie verloopt in %duur% minuten.',
-      'sessieEindeBoodschap': 'Uw sessie is verlopen',
-      'afterExpiration': $.noop
-    },
-        options = $.extend(settings, options),
-        intervalId,
-        sessieTijdOver, // in seconden
-        timerTekst,
-        verlengenControl = $(verlengen),
-        warnExpirationDialog = null,
-        noMoreWarning = false;
+/*global jQuery */
+(function ($) {
 
-    verlengenControl.click(function() {
-      sessieTijdOver = parseInt(options.sessieDuur) * 60;
-      berichtSessie();      
-    });
-    
-    var resetTimer = function() {
-      if (intervalId) {
-        sessieTijdOver = parseInt(options.sessieDuur) * 60;        
-      }
-    };
-    
-    var makeWarnExpiprationDialog = function() {
-      return $(warnDialogContent.replace('%duur%', options.sessieDuur)).dialog({
-        autoOpen: false,
-        title: warnDialogTitle.replace('%duur%', options.waarschuwingsGrens),
-        modal: true,
-        buttons: [
-        {
-          text:'OK',
-          click: function() {
-            resetTimer();
-            $(this).dialog('close');
-            berichtSessie();
-            noMoreWarning = false;
+  'use strict';
+
+  $.fn.sessietimer = function (p_options, verlengen, warnDialogContent, warnDialogTitle) {
+
+    var
+      settings = {
+        'bijwerkInterval': 1, // in seconden
+        'sessieDuur': 15,  // in minuten
+        'waarschuwingsGrens': 3, // in minuten (0 = niet waarschuwen)
+        'sessieAanBoodschap': 'Uw sessie verloopt in %duur% minuten.',
+        'sessieEindeBoodschap': 'Uw sessie is verlopen',
+        'afterExpiration': $.noop
+      },
+      noMoreWarning = false,
+      sessieTijdOver, // in seconden
+      options = $.extend(settings, p_options),
+      timerTekst,
+      intervalId,
+      warnExpirationDialog = null,
+
+      resetTimer = function () {
+        if (intervalId) {
+          sessieTijdOver = parseInt(options.sessieDuur, 10) * 60;
+        }
+      },
+
+      makeWarnExpirationDialog = function () {
+        return $(warnDialogContent.replace('%duur%', options.sessieDuur)).dialog({
+          autoOpen: false,
+          title: warnDialogTitle.replace('%duur%', options.waarschuwingsGrens),
+          modal: true,
+          buttons: [
+            {
+              text: 'OK',
+              click: function () {
+                resetTimer();
+                $(this).dialog('close');
+                berichtSessie();
+                noMoreWarning = false;
+              },
+              deflt: true
+            },
+            {
+              text: 'Annuleer',
+              click: function () {
+                $(this).dialog('close');
+              }
+            }
+          ],
+          open: function () {
+            jQuery(this).parents('ui-dialog-buttonpane button:eq(0)').focus();
           },
-          deflt: true
-        },
-          {
-          text:'Annuleer',
-          click: function() {
-            $(this).dialog('close');
+          close: function () {
+            noMoreWarning = true;
+          }
+        });
+      },
+
+      callUIDialogWarnExpiration = function () {
+        if (warnExpirationDialog === null) {
+          warnExpirationDialog = makeWarnExpirationDialog();
+        }
+        warnExpirationDialog.dialog('open');
+      },
+
+      berichtSessie = function () {
+        var seconden, minuten, tijd, msg;
+
+        seconden = sessieTijdOver % 60;
+        if (seconden < 10) {
+          seconden = '0' + seconden;
+        }
+        minuten = (sessieTijdOver - seconden) / 60;
+        tijd  = minuten + ':' + seconden;
+        msg = options.sessieAanBoodschap.replace('%duur%', tijd);
+
+        timerTekst.html(msg);
+        $('.timer-tekst').html(tijd); // vul het verstrijken der tijd ook in in de dialoog
+
+        sessieTijdOver -= 1;
+        if (sessieTijdOver === 0) {
+          timerTekst.html(options.sessieEindeBoodschap);
+          clearInterval(intervalId);
+          options.afterExpiration();
+          if (warnExpirationDialog) {
+            warnExpirationDialog.dialog('close');
           }
         }
-      ],
-      open: function() {
-        jQuery(this).parents('ui-dialog-buttonpane button:eq(0)').focus();    
-      },
-      close: function() {
-        noMoreWarning = true;        
-      }
-      });      
-    };
-    
-    var callUIDialogWarnExpiration = function() {
-      if (warnExpirationDialog == null) {
-        warnExpirationDialog = makeWarnExpiprationDialog();
-      }
-      warnExpirationDialog.dialog('open');
-    };
-
-    var berichtSessie = function() {
-      var seconden = sessieTijdOver % 60,
-          minuten = (sessieTijdOver - seconden) / 60;
-          
-      if (seconden < 10) {
-        seconden = '0' + seconden;
-      }
-      var tijd  = minuten + ':' + seconden,
-          msg = options.sessieAanBoodschap.replace('%duur%', tijd);
-          
-      timerTekst.html(msg);
-      $('.timer-tekst').html(tijd); // vul het verstrijken der tijd ook in in de dialoog
-      sessieTijdOver--;
-      if (sessieTijdOver == 0) {
-        timerTekst.html(options.sessieEindeBoodschap);
-        clearInterval(intervalId);
-        options.afterExpiration();
-        if (warnExpirationDialog) {
-          warnExpirationDialog.dialog('close');
+        if (options.waarschuwingsGrens > 0 && minuten < options.waarschuwingsGrens && !noMoreWarning) {
+          callUIDialogWarnExpiration();
         }
-      }
-      if (options.waarschuwingsGrens > 0 && minuten < options.waarschuwingsGrens && !noMoreWarning) {
-        callUIDialogWarnExpiration();        
-      }
-    };
-    
-    return this.each(function() {
+      };
+
+    $(verlengen).click(function () {
+      sessieTijdOver = parseInt(options.sessieDuur, 10) * 60;
+      berichtSessie();
+    });
+
+    return this.each(function () {
       timerTekst = $(this);
-      sessieTijdOver = parseInt(options.sessieDuur) * 60;
+      sessieTijdOver = parseInt(options.sessieDuur, 10) * 60;
       berichtSessie();
       intervalId = setInterval(berichtSessie, options.bijwerkInterval * 1000);
-    });    
+    });
   }
-})(jQuery);
+}(jQuery));
 
--- js/main.js
+++ js/main.js
@@ -1,21 +1,46 @@
-$(function() {
+/*global $, jsonData, document */
+$(function () {
 
-  var selectedAction;
+  'use strict';
+
+  var selectedAction,
+    action,
+
+    // initialisaties
+    $test = $('#test'),
+
+    // Inhoud van de dialoog die waarschuwt dat de tijd bijna verstreken is.
+    warnDialogContent =
+      '<div class="warning-expiration" style="display:none">' +
+      '<p>Als u %duur% minuten niets heeft opgeslagen, wordt u automatisch uitgelogd. ' +
+      'Dit is ingesteld om uw gegevens zo goed mogelijk te beveiligen.' +
+      'Niet opgeslagen gegevens zullen verloren gaan.</p>' +
+      '<p>Wilt u ingelogd blijven? Klik dan op de knop OK.</p>' +
+      '</div>',
+
+    // Titel van de dialoog die waarschuwt dat de tijd bijna verstreken is.
+    warnDialogTitle =
+      'Over <span class="timer-tekst">%duur%</span> minuten wordt u automatisch' +
+      ' uitgelogd!';
+
 
   // vul teksten in voor voorbeeld
   function displayDemo(action) {
-    var data = jsonData[action];
+    var data = jsonData[action],
+      html = '',
+      i;
+
     $test.removeClass('selected');
 
     $('#content-header').text(data.header);
+    $('#beschrijving-html').html(data.descriptionHtml);
     $('#beschrijving-js').html(data.descriptionJs);
     $('pre#html').html(data.html);
     $('pre#js').html(data.js);
     $('#voorbeeld').html(data.voorbeeld);
     $('#jstest').toggle(data.schakelaar);
 
-    var html = '', i = 0;
-    for(; i < data.features.length; i++) {
+    for (i = 0; i < data.features.length; i += 1) {
       html += '<li>' + data.features[i] + '</li>';
     }
     $('#kenmerken').html(html);
@@ -23,83 +48,72 @@ $(function() {
     selectedAction = action;
     $.cookie('selectedAction', selectedAction);
 
-    if (action == 'wisselcitaat') {
+    if (action === 'wisselcitaat') {
       $('.quotes').randomquote();
     }
   }
 
   // handle navigation
-  $('.action').click(function(){
-    displayDemo(this.id)
+  $('.action').click(function () {
+    displayDemo(this.id);
   });
 
-  // Inhoud en titel van de dialoog die waarschuwt dat de tijd bijna verstreken is.
-  var warnDialogContent =
-'<div class="warning-expiration" style="display:none">' +
-'<p>Als u %duur% minuten niets heeft opgeslagen, wordt u automatisch uitgelogd. ' +
-'Dit is ingesteld om uw gegevens zo goed mogelijk te beveiligen. Niet opgeslagen gegevens zullen verloren gaan.</p>' +
-'<p>Wilt u ingelogd blijven? Klik dan op de knop OK.</p>' +
-'</div>',
-      warnDialogTitle =
-  'Over <span class="timer-tekst">%duur%</span> minuten wordt u automatisch uitgelogd!';
-
   // handle javascript schakelaar
-  var $test = $('#test');
-  $test.click(function() {
+  $test.click(function () {
     if ($test.hasClass('selected')) {
       $('#carrousel').carrousel('close');
       document.location.reload();
-    }
-    else {
+    } else {
       $test.addClass('selected');
 
       // actie hangt af van gekozen voorbeeld
-      switch(selectedAction) {
-        case 'tekenteller':
+      switch (selectedAction) {
+      case 'tekenteller':
 
-          // verberg statische melding
-          $('.max-warning').hide();
+        // verberg statische melding
+        $('.max-warning').hide();
 
-          // init jqEasyCounter
-          $('#testtextarea').jqEasyCounter({
-            msgString: 'U kunt nog %left% tekens invoeren.',
-            msgSingleCharString: 'U kunt nog 1 teken invoeren.',
-            maxChars: 100,
-            maxCharsWarning: 90
-          });
-          break;
-        case 'carrousel':
-          $('#carrouselcontainer').carrousel({
-            carrouselSlideDuration: 3000
-          });
-          break;
-        case 'timer':
-          $('#sessie-timer-tekst').sessietimer({
-            'sessieDuur' : $('#sessieduur').val(),
-            'waarschuwingsGrens' : $('#waarschuwingslimiet').val(),
-            'afterExpiration': function() {
-                $('.options-expiration').hide();
-              }
-            }, '#sessie-timer-verlengen', warnDialogContent, warnDialogTitle
+        // init jqEasyCounter
+        $('#testtextarea').jqEasyCounter({
+          msgString: 'U kunt nog %left% tekens invoeren.',
+          msgSingleCharString: 'U kunt nog 1 teken invoeren.',
+          maxChars: 100,
+          maxCharsWarning: 90
+        });
+        break;
+      case 'carrousel':
+        $('#carrouselcontainer').carrousel({
+          carrouselSlideDuration: 3000
+        });
+        break;
+      case 'timer':
+        $('#sessie-timer-tekst').sessietimer({
+          'sessieDuur' : $('#sessieduur').val(),
+          'waarschuwingsGrens' : $('#waarschuwingslimiet').val(),
+          'afterExpiration': function () {
+            $('.options-expiration').hide();
+          }
+        }, '#sessie-timer-verlengen', warnDialogContent, warnDialogTitle
           );
-          break;
-        case 'validatie':
-
-          break;
-        case 'tekstlade':
-          $('#tips').textdrawer();
-          break;
-        case 'wisselcitaat':
-          // geen schakelaar op deze demo-pagina
-          break;
+        break;
+      case 'validatie':
+        // nog niet gebouwd
+        break;
+      case 'tekstlade':
+        $('#tips').textdrawer();
+        break;
+      case 'wisselcitaat':
+        // geen schakelaar op deze demo-pagina
+        break;
       }
     }
   });
 
-    var action = 'tekenteller';
-    if ($.cookie('selectedAction') && typeof $.cookie('selectedAction') != 'undefined') {
-      action = $.cookie('selectedAction');
-    }
-    displayDemo(action);
+  if ($.cookie('selectedAction') && $.cookie('selectedAction') !== 'undefined') {
+    action = $.cookie('selectedAction');
+  } else {
+    action = 'tekenteller';
+  }
+  displayDemo(action);
 
 });
